dfc40ae2623dda1820d3d1cf9ad1686a5bfa2d00
marquis-code <abahmarquis@gmail.com>
2026-06-16 21:39:52 +0100

    Change MongoDB connection string to empty
    Updated MongoDB connection string to an empty value.

--- scripts/test-chat.js
+++ scripts/test-chat.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const mongoose = require('mongoose');
 
 async function test() {
-  await mongoose.connect('mongodb+srv://developer:7K4jN0m2ZkVg3uR@cluster0.a1b2c.mongodb.net/erranders?retryWrites=true&w=majority', { useNewUrlParser: true });
+  await mongoose.connect('', { useNewUrlParser: true });
   const db = mongoose.connection;
   // wait we don't have to connect. The server is running locally on 3000. 
   // Let me just query it by inserting a token.
